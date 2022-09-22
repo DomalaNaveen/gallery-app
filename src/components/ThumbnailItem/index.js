934da0c2fb8b5c6511ef13d1a324009396b90cba
root <naveen9603737739@gmail.com>
2022-09-22 13:30:46 +0000

    gallery app

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {imageDetails, isActive, setActiveThumbnailId} = props
+  const {thumbnailUrl, thumbnailAltText, id} = imageDetails
+  const thumbnailClassName = isActive ? `thumbnail active` : `thumbnail`
+
+  const onClickThumbnail = () => {
+    setActiveThumbnailId(id)
+  }
+  return (
+    <li>
+      <button type="button" className="button" onClick={onClickThumbnail}>
+        <img
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+          className={thumbnailClassName}
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
